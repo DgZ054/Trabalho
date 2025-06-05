36803cfb03dba8e7d5308ecd2158fc09be7ad3ff
DgZ054 <douglasboff2007@gmail.com>
2025-06-05 16:20:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('botao').addEventListener('click', function() {
+    document.getElementById('mensagem').textContent = "Seja Bem-vindo a minha Autobiografia resumida. Espero que goste :)"
+})
+/*Obs: tive este codigo eu estava com dificuldades de desenvolver... pedi ajuda para um amigo mais experiente na area*/
